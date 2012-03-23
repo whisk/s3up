bb74d08db55280dee70f23154703fbe26edecf2b
Your Name <alex@svetkin.ru>
2012-03-24 02:16:04 +0400

    better compare

--- benchmark.rb
+++ benchmark.rb
@@ -6,23 +6,24 @@ NUM = 10
 FILES = ['1k.dat', '64k.dat', '256k.dat', '512k.dat', '1m.dat', '10m.dat', '50m.dat']
 s3 = S3up.new('aws.yml')
 
-[:upload, :multipart_upload].reverse.each do |method|
-  print tmp = "Benchmarking method: #{method.to_s}\n"
-  print "=" * tmp.size + "\n"
-  FILES.each do |filename|
-    next unless File.exist?(filename)
-    t_total = 0.0
-    filesize = File.size(filename)
-    print "Benchmarking #{filename} (#{filesize} bytes) "
-    NUM.times do |i|
-      print '.'
+FILES.each do |filename|
+  next unless File.exist?(filename)
+  filesize = File.size(filename)
+  t_total = {}
+  print tmp = "Benchmarking #{filename} (#{filesize} bytes) "
+  NUM.times do |i|
+    print '.'
+    [:upload, :multipart_upload].reverse.each do |method|
       t0 = Time.now.to_f
       s3.send(method, filename, nil)
-      t_total += Time.now.to_f - t0
+      t_total[method] ||= 0.0
+      t_total[method] += Time.now.to_f - t0
     end
-    print "\n"
-    printf tmp = "Avg. upload time: %0.4fs (%0.4fMb/s)\n", t_total / NUM, filesize / (t_total / NUM) / 1024 / 1024
-    print "-" * tmp.size + "\n"
   end
+  print "\n"
+  t_total.each do |method, t|
+    printf tmp = "#{method.to_s} avg. upload time: %0.2fs (%0.2fMb/s)\n", t / NUM, filesize / (t / NUM) / 1024 / 1024
+  end
+  print '-' * 40 + "\n"
 end
 
