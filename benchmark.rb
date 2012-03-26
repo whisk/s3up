2c48f8a9d37b085a4914df81f58fce22544ed952
Your Name <alex@svetkin.ru>
2012-03-26 14:13:45 +0400

    refactoring

--- benchmark.rb
+++ benchmark.rb
@@ -2,28 +2,41 @@ require 'lib/s3up.rb'
 $stdout.sync = true
 
 
-NUM = 10
+NUM = (ENV['NUM'] || 10).to_i
 FILES = ARGV
 s3 = S3up.new('aws.yml')
 
+methods = [:upload, :multipart_upload, :threaded_upload]
+compare_with_method = :upload
+
 FILES.each do |filename|
-  next unless File.exist?(filename)
-  filesize = File.size(filename)
-  t_total = {}
-  print tmp = "Benchmarking #{filename} (#{filesize} bytes) "
-  NUM.times do |i|
-    print '.'
-    [:upload, :multipart_upload].reverse.each do |method|
-      t0 = Time.now.to_f
-      s3.send(method, filename, nil)
-      t_total[method] ||= 0.0
-      t_total[method] += Time.now.to_f - t0
+  begin
+    next unless File.exist?(filename)
+    filesize = File.size(filename)
+    t_total = {}
+    print tmp = "Benchmarking #{filename} (#{filesize} bytes) "
+    NUM.times do |i|
+      print '.'
+      methods.reverse.each do |method|
+        t0 = Time.now.to_f
+        s3.send(method, filename, nil)
+        t_total[method] ||= 0.0
+        t_total[method] += Time.now.to_f - t0
+      end
     end
+  rescue Interrupt => e
+    puts "Aborted by user"
+    exit
+  ensure
+    print "\n"
+    methods.each do |method|
+      t_total[method] ||= 0
+      t = t_total[method] / NUM
+      ratio = (t_total[compare_with_method] / NUM) / t rescue 0
+      speed = filesize / t / 1024 / 1024 rescue 0
+      printf tmp = "#{method.to_s} avg. upload time: %0.2fs (%0.2fMb/s, %0.2fX)\n", t, speed, ratio
+    end
+    print '-' * 40 + "\n"
   end
-  print "\n"
-  t_total.each do |method, t|
-    printf tmp = "#{method.to_s} avg. upload time: %0.2fs (%0.2fMb/s)\n", t / NUM, filesize / (t / NUM) / 1024 / 1024
-  end
-  print '-' * 40 + "\n"
 end
 
--- lib/s3up.rb
+++ lib/s3up.rb
@@ -12,6 +12,7 @@ class S3up
       :secret_access_key => config['secret_access_key']).buckets[config['bucket_name']]
   end
 
+  # простая закачка
   def upload(src, dst = nil, check_md5 = false)
     dst ||= File.basename(src)
 
@@ -24,7 +25,47 @@ class S3up
     s3_file
   end
 
+  # закачка частями
   def multipart_upload(src, dst = nil, check_md5 = false)
+    dst ||= File.basename(src)
+    s3_file = bucket.objects[dst]
+
+    # открываем локальный src файл для чтения
+    src_io = File.open(src, 'rb')
+    # несколько полезных счетчиков
+    read_size = 0
+    uploaded_size = 0
+    parts = 0
+    # размер файла пригодится
+    src_size = File.size(src)
+    # начинаем закачку частями
+    s3_file = s3_file.multipart_upload({:acl => :public_read}) do |upload|
+      while read_size < src_size
+        # считаем последовательность байт заданного размера из файла в буфер
+        buff = src_io.readpartial(config['part_size'])
+        # увеличим счетчики
+        read_size += buff.size
+        part_number = parts += 1
+
+        # собственно, закачаем считанные данные на S3
+        upload.add_part :data => buff, :part_number => part_number
+        uploaded_size += buff.size
+
+        # вызовем блок и передадим ему информацию о прогрессе закачки
+        yield(uploaded_size.to_f / src_size) if block_given?
+      end
+    end
+    # закроем исходный файл
+    src_io.close
+
+    raise "S3: Error uploading file '#{src}' -> '#{dst}" unless s3_file.instance_of?(AWS::S3::S3Object)
+    do_check_md5(src, s3_file) if check_md5
+
+    s3_file
+  end
+
+  # многопоточная закачка частями
+  def threaded_upload(src, dst = nil, check_md5 = false)
     dst ||= File.basename(src)
     s3_file = bucket.objects[dst]
     
@@ -34,26 +75,44 @@ class S3up
     parts = 0
     src_size = File.size(src)
     s3_file = s3_file.multipart_upload({:acl => :public_read}) do |upload|
+      # заведем массив для сохранения информации о потоках
       upload_threads = []
+      # создадим мьютекс (или семафор), чтобы избежать “гонок”
       mutex = Mutex.new
+      # число потоков - не больше максимально разрешенного значения
       max_threads = [config['threads_count'], (src_size.to_f / config['part_size']).ceil].min
+      # в цикле создадим требуемое количество потоков
       max_threads.times do |thread_number|
         upload_threads << (Thread.new do
+          # мы в свежесозданном потоке
           while true
+            # входим в участок кода, в котором одновременно может находится только один поток
+            # выставляем блокировку
             mutex.lock
+            # прерываем цикл, если все данные уже прочитаны из файла
             break unless read_size < src_size
+            # считаем последовательность байт заданного размера из файла в буфер
             buff = src_io.readpartial(config['part_size'])
+            # увеличим счетчики
             read_size += buff.size
             part_number = parts += 1
+            # снимаем блокировку
             mutex.unlock
 
+            # собственно, закачаем считанные данные на S3
             upload.add_part :data => buff, :part_number => part_number
-            uploaded_size += buff.size
 
+            # увеличение счетчика
+            mutex.lock
+            uploaded_size += buff.size
+            mutex.unlock
+
+            # вызовем блок и передадим ему информацию о прогрессе закачки
             yield(uploaded_size.to_f / src_size) if block_given?
           end
         end)
       end
+      # дожидаемся завершения работы всех потоков
       upload_threads.each{|thread| thread.join}
     end
     src_io.close
